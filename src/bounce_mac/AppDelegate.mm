7d614373a58dc2792960cffa7b671fcd23606bd2
Daniel Wentzel <daniel.wentzel@gmail.com>
2013-05-16 23:52:42 +0200

    Fixed warning for draw in app delegate

--- src/bounce_mac/AppDelegate.mm
+++ src/bounce_mac/AppDelegate.mm
@@ -4,7 +4,12 @@
 #import "bounce/EntryPoint.h"
 #import "MacEventManager.h"
 
+@interface MyApplicationDelegate()
+- (void) draw;
+@end
+
 @implementation MyApplicationDelegate : NSObject
+
 @synthesize window;
 @synthesize openGLContext;
 
@@ -35,31 +40,15 @@ void draw(void* context) {
 - (void)draw {
     NSLog(@"Drawing...");
     
-    [self.openGLContext makeCurrentContext];
-    
-    //glClearColor(1, 0, 1, 1);
-    //glClear(GL_COLOR_BUFFER_BIT);
+    //[self.openGLContext makeCurrentContext];
     
     [self.openGLContext flushBuffer];
 }
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    //NSRect mainDisplayRect = [[NSScreen mainScreen] frame]; // I'm going to make a full screen view.
-    
-    //NSRect frame = NSMakeRect(0, 0, 200, 200);
-    
-    //self.window = [[[NSWindow alloc] initWithContentRect:frame
-    //                                         styleMask:NSTitledWindowMask backing:NSBackingStoreBuffered defer:NO]
-    //             autorelease];
-    //[self.window cascadeTopLeftFromPoint:NSMakePoint(20,20)];
-    
-    //id appName = [[NSProcessInfo processInfo] processName];
-    //[self.window setTitle:appName];
-    //[self.window makeKeyAndOrderFront:nil];
-    
     NSOpenGLPixelFormatAttribute attr[] = {
         NSOpenGLPFAOpenGLProfile,
-        NSOpenGLProfileVersion3_2Core, // Needed if using opengl 3.2 you can comment this line out to use the old version.
+        NSOpenGLProfileVersion3_2Core,
         NSOpenGLPFAColorSize,     24,
         NSOpenGLPFAAlphaSize,     8,
         NSOpenGLPFAAccelerated,
@@ -67,28 +56,12 @@ void draw(void* context) {
         0
     };
     
-    NSOpenGLPixelFormat *pix = [[NSOpenGLPixelFormat alloc] initWithAttributes:attr];
-    self.openGLContext = [[NSOpenGLContext alloc] initWithFormat: pix shareContext: nil];
+    NSOpenGLPixelFormat *pixelFormat = [[NSOpenGLPixelFormat alloc] initWithAttributes:attr];
+    
+    self.openGLContext = [[NSOpenGLContext alloc] initWithFormat: pixelFormat shareContext: nil];
     [self.openGLContext setView:[self.window contentView]];
-    //self.view = [[OpenGLViewCoreProfile alloc] initWithFrame:self.window.frame pixelFormat:pix];
-    
-    //self.view = [[OpenGLViewCoreProfile alloc] initWithPixelFormat:pix];
-    
-    // Below shows how to make the view fullscreen. But you could just add to the contact view of any window.
-    //self.window = [[NSWindow alloc] initWithContentRect:mainDisplayRect
-    //                                          styleMask:NSBorderlessWindowMask
-    //                                            backing:NSBackingStoreBuffered
-    //                                              defer:YES];
-    
-    //self.window.opaque = YES;
-    //self.window.hidesOnDeactivate = YES;
-    //self.window.level = NSMainMenuWindowLevel + 1; // Show window above main menu.
-    //self.window.contentView = self.view;
-    
-    //NSOpenGLContext* context = [NSOpenGLContext currentContext];
-    //[context makeCurrentContext];
-    //[self.window makeKeyAndOrderFront:self]; // Display window.
     [self.openGLContext makeCurrentContext];
+    
     bounce::EntryPoint entryPoint;
     bounce_mac::MacEventManager eventManager;
     entryPoint.run(eventManager, &draw, self);
