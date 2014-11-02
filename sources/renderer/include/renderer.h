7eeb37b41440500f96944c4e04a0d68050421d1c
Daniel Wentzel <daniel.wentzel@gmail.com>
2014-11-02 13:21:21 +0100

    added renderer

---
+++ sources/renderer/include/renderer.h
@@ -0,0 +1,17 @@
+#ifndef BOUNCE_RENDERER_RENDERER_H_
+#define BOUNCE_RENDERER_RENDERER_H_
+
+namespace bounce
+{
+    
+    class Renderer
+    {
+    private:
+        
+    public:
+        
+    };
+    
+}
+
+#endif // BOUNCE_RENDERER_RENDERER_H_
