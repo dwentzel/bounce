83179124e5e2a8c87202b38fcfe6539bb19e80b9
Daniel Wentzel <daniel.wentzel@gmail.com>
2014-11-07 21:51:29 +0100

    cleanup

--- sources/bounce/test/keyboard_state_test.cc
+++ sources/bounce/test/keyboard_state_test.cc
@@ -2,50 +2,50 @@
 #include "keyboard_state.h"
 
 namespace bounce {
-namespace tests {
-
-class KeyboardStateTest : public ::testing::Test {
-protected:
-	// You can remove any or all of the following functions if its body
-	// is empty.
-
-	KeyboardStateTest() {
-		// You can do set-up work for each test here.
-	}
-
-	virtual ~KeyboardStateTest() {
-		// You can do clean-up work that doesn't throw exceptions here.
-	}
-
-	// If the constructor and destructor are not enough for setting up
-	// and cleaning up each test, you can define the following methods:
-
-	virtual void SetUp() {
-		// Code here will be called immediately after the constructor (right
-		// before each test).
-	}
-
-	virtual void TearDown() {
-		// Code here will be called immediately after each test (right
-		// before the destructor).
-	}
-
-	KeyboardState subject;
-};
-
-TEST_F(KeyboardStateTest, Key_A_is_up_by_default) {
-	EXPECT_FALSE(subject.IsDown(Key::A));
-}
-
-TEST_F(KeyboardStateTest, Key_A_is_down_after_keydown_event) {
-	Keysym keySym;
-	keySym.sym = Key::A;
-
-	KeydownEvent event(keySym);
-	subject.ProcessEvent(event);
-	EXPECT_TRUE(subject.IsDown(Key::A));
-}
-
-
-}
+    namespace tests {
+        
+        class KeyboardStateTest : public ::testing::Test {
+        protected:
+            // You can remove any or all of the following functions if its body
+            // is empty.
+            
+            KeyboardStateTest() {
+                // You can do set-up work for each test here.
+            }
+            
+            virtual ~KeyboardStateTest() {
+                // You can do clean-up work that doesn't throw exceptions here.
+            }
+            
+            // If the constructor and destructor are not enough for setting up
+            // and cleaning up each test, you can define the following methods:
+            
+            virtual void SetUp() {
+                // Code here will be called immediately after the constructor (right
+                // before each test).
+            }
+            
+            virtual void TearDown() {
+                // Code here will be called immediately after each test (right
+                // before the destructor).
+            }
+            
+            KeyboardState subject;
+        };
+        
+        TEST_F(KeyboardStateTest, Key_A_is_up_by_default) {
+            EXPECT_FALSE(subject.IsDown(KEY_A));
+        }
+        
+        TEST_F(KeyboardStateTest, Key_A_is_down_after_keydown_event) {
+            Keysym keySym;
+            keySym.sym = KEY_A;
+            
+            KeydownEvent event(keySym);
+            subject.ProcessEvent(event);
+            EXPECT_TRUE(subject.IsDown(KEY_A));
+        }
+        
+        
+    }
 }
